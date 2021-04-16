d9eec21ddbeec879a91363bb1d6a3355bef70896
boxmars <2803726925@qq.com>
2021-04-16 22:38:00 +0800

    Update

--- src/dev/boxz/MinimumDepthofBinaryTree.java
+++ src/dev/boxz/MinimumDepthofBinaryTree.java
@@ -1,5 +1,8 @@
 package dev.boxz;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author Box
  * @see <a href="https://leetcode.com/problems/minimum-depth-of-binary-tree/">MinimumDepthofBinaryTree</a>
@@ -7,7 +10,13 @@ package dev.boxz;
 public class MinimumDepthofBinaryTree {
 
     public static void main (String[] args) {
-
+        TreeNode root4=new TreeNode (4);
+        TreeNode root5=new TreeNode (5);
+        TreeNode root3=new TreeNode (3);
+        TreeNode root2=new TreeNode (2,root4,root5);
+        TreeNode root1=new TreeNode (1,root2,root3);
+        MinimumDepthofBinaryTree minimumDepthofBinaryTree=new MinimumDepthofBinaryTree ();
+        minimumDepthofBinaryTree.minDepth (root1);
     }
     public static class TreeNode {
 
@@ -30,8 +39,28 @@ public class MinimumDepthofBinaryTree {
     }
 
     public int minDepth(TreeNode root) {
-
-
-        return 0;
+        if (root==null){
+            return 0;
+        }
+        Queue<TreeNode> queue=new LinkedList<> ();
+        queue.add (root);
+        int depth=1;
+        while (!queue.isEmpty ()){
+            int length=queue.size ();
+            for (int i=0;i<length;i++){
+                TreeNode treeNode=queue.poll ();
+                if (treeNode.left==null&&treeNode.right==null){
+                    return depth;
+                }
+                if (treeNode.left!=null){
+                    queue.add (treeNode.left);
+                }
+                if (treeNode.right!=null){
+                    queue.add (treeNode.right);
+                }
+            }
+            depth+=1;
+        }
+        return depth;
     }
 }
